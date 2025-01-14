328a90f9f47b968286ecda1dac245c1297b49638
febreno <felipesugisawa1@gmail.com>
2025-01-13 21:54:31 -0300

    new

--- ac-front/src/components/list/product/downloadProduct/index.js
+++ ac-front/src/components/list/product/downloadProduct/index.js
@@ -3,34 +3,34 @@ import * as Styled from './style';
 import { DownloadProductCard } from '../../../cards/downloadProduct';
 
 const list = [
-    {
-      name: 'João Silva',
-      email: 'joao.silva@email.com',
-      company: 'Empresa X',
-      phone_number: '(11) 98765-4321',
-      product_id: '12345',
-      product_name: 'Produto A',
-      created_at: '2025-01-10 14:20:30.123',
-    },
-    {
-      name: 'Maria Oliveira',
-      email: 'maria.oliveira@email.com',
-      company: 'Empresa Y',
-      phone_number: '(21) 99876-5432',
-      product_id: '67890',
-      product_name: 'Produto B',
-      created_at: '2025-01-11 09:15:05.456',
-    },
-    {
-      name: 'Carlos Souza',
-      email: 'carlos.souza@email.com',
-      company: 'Empresa Z',
-      phone_number: '(31) 99887-6543',
-      product_id: '11223',
-      product_name: 'Produto C',
-      created_at: '2025-01-12 11:30:45.789',
-    },
-  ];
+  {
+    name: 'João Silva',
+    email: 'joao.silva@email.com',
+    company: 'Empresa X',
+    phone_number: '(11) 98765-4321',
+    product_id: '12345',
+    product_name: 'Produto A',
+    created_at: '2025-01-10 14:20:30.123',
+  },
+  {
+    name: 'Maria Oliveira',
+    email: 'maria.oliveira@email.com',
+    company: 'Empresa Y',
+    phone_number: '(21) 99876-5432',
+    product_id: '67890',
+    product_name: 'Produto B',
+    created_at: '2025-01-11 09:15:05.456',
+  },
+  {
+    name: 'Carlos Souza',
+    email: 'carlos.souza@email.com',
+    company: 'Empresa Z',
+    phone_number: '(31) 99887-6543',
+    product_id: '11223',
+    product_name: 'Produto C',
+    created_at: '2025-01-12 11:30:45.789',
+  },
+];
 
 
 export function DownloadProductCardList({ list }) {
@@ -59,7 +59,7 @@ export function DownloadProductCardList({ list }) {
             textAlign: 'center',
           }}
         >
-          Nenhum motorista encontrado.
+          Nenhum histórico de download.
         </p>
       )}
     </Styled.Cards>
